ccade2d6ba1d2975561558de35a4827f3c0f80c1
marianacelyvelasquez <cely.mariana@gmail.com>
2023-12-16 16:16:24 +0000

    Add update_swag (half done)

--- experiments/SWAG/SWAG.py
+++ experiments/SWAG/SWAG.py
@@ -1,5 +1,8 @@
 import enum
 import torch
+import typing
+import collections
+
 
 class InferenceMode(enum.Enum):
     """
@@ -18,11 +21,11 @@ class SWAGInference(object):
         inference_mode: InferenceMode = InferenceMode.SWAG_DIAGONAL,
 
         #MARI some extra epochs to collect all the weights - 30 “different models“
-        #MARI then you average over something something
+        # then you average over something something
         swag_epochs: int = 30,
 
         swag_learning_rate: float = 0.045,
-        #MARI how often you update the swag statistics but we keep it as 1 (simple)
+        #MARI how often (in #epochs) you update the swag statistics - we keep it as 1 (simple)
         #MARI build covariance stuff
         #MARI algo: bayesian model algorithm with SWAG
         swag_update_freq: int = 1, 
@@ -51,16 +54,67 @@ class SWAGInference(object):
         self.train_dataset = torch.utils.data.TensorDataset(train_xs) 
 
         # SWAG-diagonal
-        # TODO(1): create attributes for SWAG-diagonal
-        #  Hint: self._create_weight_copy() creates an all-zero copy of the weights
-        #  as a dictionary that maps from weight name to values.
-        #  Hint: you never need to consider the full vector of weights,
+        self.weight_copy = self._create_weight_copy()
+        # MARI : did Hint1, must understand if Hint2 is done as well or another tricky thing
+        #  Hint 2: you never need to consider the full vector of weights,
         #  but can always act on per-layer weights (in the format that _create_weight_copy() returns)
 
         # Full SWAG
-        # TODO(2): create attributes for SWAG-diagonal
-        #  Hint: check collections.deque
+        self.weight_copies_full = collections.deque(maxlen=swag_epochs * swag_update_freq)
+        # MARI: This deque will store multiple weight copies over the course of training
 
         # Calibration, prediction, and other attributes
         # TODO(2): create additional attributes, e.g., for calibration
         self._prediction_threshold = None  # this is an example, feel free to be creative
+
+
+    def update_swag(self) -> None:
+        """
+        Update SWAG statistics with the current weights of self.network. (here the CNN)
+        """
+
+        # Create a copy of the current network weights
+        # MARI PASCAL TODO: check that this is compatible with the CNN implementation (named_parameters field)
+        current_params = {name: param.detach() for name, param in self.network.named_parameters()}
+
+        # SWAG-diagonal
+        for name, param in current_params.items():
+            # TODO(1): update SWAG-diagonal attributes for weight `name` using `current_params` and `param`
+            raise NotImplementedError("Update SWAG-diagonal statistics")
+
+        # Full SWAG
+        if self.inference_mode == InferenceMode.SWAG_FULL:
+            # TODO(2): update full SWAG attributes for weight `name` using `current_params` and `param`
+            raise NotImplementedError("Update full SWAG statistics")
+
+
+    def _create_weight_copy(self) -> typing.Dict[str, torch.Tensor]:
+        """Create an all-zero copy of the network weights as a dictionary that maps name -> weight"""
+        return {
+            name: torch.zeros_like(param, requires_grad=False) # dictionary {name: zeroes-matrix}
+            for name, param in self.network.named_parameters()
+            #MARI get weights; tensors usually are 4-dimensional [dim 1: #samples, dim 2: channels, dims 3-4: kernel size]
+        }
+    
+    def update_swag(self) -> None:
+        """
+        Update SWAG statistics with the current weights of self.network.
+        """
+
+        # Create a copy of the current network weights
+        current_params = {name: param.detach() for name, param in self.network.named_parameters()}
+
+        # SWAG-diagonal
+        for name, param in current_params.items():
+            #MARI TODO: update SWAG-diagonal attributes for weight `name` using `current_params` and `param`
+            raise NotImplementedError("Update SWAG-diagonal statistics")
+
+        # Full SWAG
+        if self.inference_mode == InferenceMode.SWAG_FULL:
+            self._update_full_swag(current_params)
+
+    def _update_full_swag(self, current_params) -> None:
+        """Update the Full SWAG weight copies during training."""
+        # Append a copy of the current weights to the deque
+        self.weight_copies_full.append(current_params)
+
