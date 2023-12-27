db70094aaae9a9d99a93c43e63cbc3f485460662
Pascal Müller <mueller.pascal@hotmail.com>
2023-12-27 15:05:49 +0100

    now ignoringwarning from metrics computation

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import warnings
+
+# We get a lot of warnings from sklearn.metrics._ranking (computing average precision)
+# because we have lots of samples without any positive labels because the
+# data has 111 classes but we only look at 24.
+# We can ignore these warnings.
+warnings.filterwarnings(
+    "ignore", category=UserWarning, module="sklearn.metrics._ranking"
+)
+
 from experiments.dilated_CNN.dilatedCNN import dilatedCNNExperiment
 
 if __name__ == "__main__":
