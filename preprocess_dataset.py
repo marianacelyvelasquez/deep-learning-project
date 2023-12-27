38952662cb95f16fb3cb2e38c3a960014f3904d5
marianacelyvelasquez <cely.mariana@gmail.com>
2023-12-27 12:50:32 +0100

    Refactor code (yet more functional)

--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -22,6 +22,64 @@ mappings = pd.read_csv("data/cinc2020/label_cinc2020_top24.csv", delimiter=",")
 labels_map = mappings["SNOMED CT Code"].values
 
 
+def make_directories_for_processed_split_data(source_dir: str) -> tuple[str, str, str]:
+    processed_dir_training = Path(source_dir).parent / "cinc2020_processed_training"
+    processed_dir_training.mkdir(exist_ok=True)
+
+    processed_dir_testing = Path(source_dir).parent / "cinc2020_processed_testing"
+    processed_dir_testing.mkdir(exist_ok=True)
+
+    processed_dir_validating = Path(source_dir).parent / "cinc2020_processed_validating"
+    processed_dir_validating.mkdir(exist_ok=True)
+
+    return processed_dir_training, processed_dir_testing, processed_dir_validating
+
+def get_record_paths_and_labels_binary_encoded_list(input_dir: str) -> tuple[list[str],list[list[int]]]:
+    # Allocate mem for records list and labels list
+    record_paths: list[str] = []
+    labels_binary_encoded_list: list[list[int]] = []
+
+    # Process each record and save to the appropriate directory
+    for dirpath, dirnames, filenames in os.walk(input_dir):
+        for filename in filenames:
+            if filename.endswith(".hea"):
+                record_path = os.path.join(dirpath, filename.split(".")[0]) # keep path + filename without extension (so no difference between .hea and .mat)
+                record_paths.append(record_path)
+                # Get labels from the content of the .hea file [MARI: sorry I went fully functional]
+                record = wfdb.rdrecord(record_path)
+                labels_binary_encoded = get_labels_binary_encoded(record)
+                labels_binary_encoded_list.append(labels_binary_encoded)
+    return record_paths, labels_binary_encoded_list
+
+def split_data_using_stratification(record_paths: list[str], labels_binary_encoded_list: list[list[int]]) -> tuple[list[str],list[str],list[str]]:
+    # Convert lists to arrays
+    X = np.array(record_paths)
+    y = np.array(labels_binary_encoded_list)
+
+    # Perform iterative stratified split
+    X_train, X_temp, y_train, y_temp = iterative_train_test_split(
+        X,
+        y,
+        test_size=0.2,  # 0.8 train ?
+        random_state=42,  # For reproducibility
+        stratify=y
+    )
+
+    X_val, X_test, y_val, y_test = iterative_train_test_split(
+        X_temp,
+        y_temp,
+        test_size=0.5,  # Half test half validate
+        random_state=42,  # For reproducibility
+        stratify=y_temp
+    )
+    
+    # Convert arrays back to lists
+    X_train = X_train.tolist()
+    X_val = X_val.tolist()
+    X_test = X_test.tolist()
+
+    return X_train, X_test, X_val
+
 def get_labels_binary_encoded(record):
     diagnosis_string = record.comments[2].split(": ", 1)[1].strip()
     diagnosis_list = diagnosis_string.split(",")
@@ -41,53 +99,15 @@ def get_labels_binary_encoded(record):
 
 
 
-def preprocess_dataset(source_dir):
+def preprocess_dataset(source_dir: str):
     #First: create directories for the split data
-    processed_dir_training = Path(source_dir).parent / "cinc2020_processed_training"
-    processed_dir_training.mkdir(exist_ok=True)
+    processed_dir_training, processed_dir_testing, processed_dir_validating = make_directories_for_processed_split_data(source_dir)
 
-    processed_dir_testing = Path(source_dir).parent / "cinc2020_processed_testing"
-    processed_dir_testing.mkdir(exist_ok=True)
+    # Get lists of paths and labels
+    record_paths, labels_binary_encoded_list = get_record_paths_and_labels_binary_encoded_list(input_dir)
 
-    processed_dir_validating = Path(source_dir).parent / "cinc2020_processed_validating"
-    processed_dir_validating.mkdir(exist_ok=True)
-
-    # Allocate stuff for records list and labels list
-    record_paths = []
-    labels_binary_encoded_list = []
-
-    # Process each record and save to the appropriate directory
-    for dirpath, dirnames, filenames in os.walk(input_dir):
-        for filename in filenames:
-            if filename.endswith(".hea"):
-                record_path = os.path.join(dirpath, filename.split(".")[0]) # keep path + filename without extension (so no difference between .hea and .mat)
-                record_paths.append(record_path)
-                # Get labels from the content of the .hea file [MARI: sorry I went fully functional]
-                record = wfdb.rdrecord(record_path)
-                labels_binary_encoded = get_labels_binary_encoded(record)
-                labels_binary_encoded_list.append(labels_binary_encoded)
-
-        # Now, stratifyyyyyyy :star:
-        # Convert lists to arrays
-    X = np.array(record_paths)
-    y = np.array(labels_binary_encoded_list)
-    
-    # Perform iterative stratified split
-    X_train, X_temp, y_train, y_temp = iterative_train_test_split(
-        X,
-        y,
-        test_size=0.2,  # 0.8 train ?
-        random_state=42,  # For reproducibility
-        stratify=y
-    )
-    
-    X_val, X_test, y_val, y_test = iterative_train_test_split(
-        X_temp,
-        y_temp,
-        test_size=0.5,  # Half test half validate
-        random_state=42,  # For reproducibility
-        stratify=y_temp
-    )
+    # Now, stratifyyyyyyy :star:
+    X_train, X_test, X_val = split_data_using_stratification(record_paths, labels_binary_encoded_list)
     
     # Initialize the processor TODO check the paths are correct lol
     processor = Processor(input_dir)
