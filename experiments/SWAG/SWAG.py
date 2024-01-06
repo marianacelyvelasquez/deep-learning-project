41809564451be133860a6dc8b322f21950f69553
marianacelyvelasquez <cely.mariana@gmail.com>
2024-01-06 14:36:24 +0100

    Add SWAGExperiment class + update script accordingly

--- experiments/SWAG/SWAG.py
+++ experiments/SWAG/SWAG.py
@@ -1,3 +1,5 @@
+from experiments.SWAG.SWAGInference import SWAGInference
+
 # 1. Load pre-pretrained model from checkpoint from dilatedCNN experiment
 
 # 2. Collect k- weights from the model i.e. e.g. make 30 epochs
@@ -9,16 +11,20 @@
 
 
 class SWAGExperiment:
-    def __init__(self) -> None:
-        # TODO: Init SwagInference
-        pass
+    def __init__(self, swag_inference: SWAGInference) -> None:
+        self.swag_inference = swag_inference
 
     def run(self) -> None:
-        # TODO: Run SwagInference::fit()
-        # TODO: Run SwagInference::calibrate()
-        # TODO: Run SwagInference::evaluate()
-        pass
+        # Run SWAGInference::fit()
+        self.swag_inference.fit_swag()
+
+        # Run SWAGInference::calibrate()
+        self.swag_inference.calibrate()
+
+        # Run SWAGInference::evaluate()
+        self.evaluate()
 
     def evaluate(self) -> None:
-        # TODO: implement evaluation
+        # Implement your evaluation logic using the SWAGInference instance
+        # You can access the necessary components through self.swag_inference
         pass
