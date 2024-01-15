b6d8e156e0c75c08e13620a564f18e5cd5af3f26
Ubuntu <ubuntu@ip-172-31-41-21.eu-central-1.compute.internal>
2024-01-15 21:43:58 +0000

    added configs

--- experiments/dilated_CNN/config.py
+++ experiments/dilated_CNN/config.py
@@ -1,14 +1,15 @@
 class Config:
-    DATA_DIR = "data/g1"
-    EARLY_STOPPING_EPOCHS = 3
+    DATA_DIR = "data/swag"
+    EARLY_STOPPING_EPOCHS = 5
     EXP_NAME = "dilated_CNN"
     LABEL_24 = "data/label_cinc2020_top24.csv"
     LEARNING_RATE = 0.001
-    LOAD_OPTIMIZER = True
+    LOAD_OPTIMIZER = False 
     MAX_NUM_EPOCHS = 35
     MIN_NUM_EPOCHS = 1
     NUM_FOLDS = 2
-    ONLY_EVAL_TEST_SET = False
-    OUTPUT_DIR = "outputJan10"
-    TEST_DATA_DIR = "data/g1_test"
-    TRAIN_DATA_DIR = "data/g1"
+    ONLY_EVAL_TEST_SET = True
+    OUTPUT_DIR = "dilated_nocv_epoch15_test"
+    TEST_DATA_DIR = "data/swag/test"
+    TRAIN_DATA_DIR = "data/swag/train"
+    DEVICE = "cpu"
